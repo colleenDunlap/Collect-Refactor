1356e5e75a70155177961bb31b8fa028d1a13130
kaftand <kaftand@gmail.com>
2018-07-19 11:29:23 +0300

    initial commit

---
+++ app/src/main/java/com/example/kaftand/entomologydatacollect/HumanLandingCatch.kt
@@ -0,0 +1,19 @@
+package com.example.kaftand.entomologydatacollect
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class HumanLandingCatch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_human_landing_catch)
+    }
+
+
+    fun collectHLCData(view: View) {
+        val value = 1
+
+    }
+}
