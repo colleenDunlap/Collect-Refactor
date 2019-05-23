954f7f536cefc53a2c63bfe01469a03ee23f534f
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-05-23 09:36:32 +0300

    It routes to a form!

--- app/src/main/java/com/example/kaftand/entomologydatacollect/Phase1/Phase1.kt
+++ app/src/main/java/com/example/kaftand/entomologydatacollect/Phase1/Phase1.kt
@@ -23,4 +23,111 @@ import com.google.gson.Gson
 import java.io.OutputStreamWriter
 import kotlin.properties.Delegates
 class Phase1 : LanguagePreservingActivity() {
+    var metaData: Phase1MetaData by Delegates.notNull()
+    var DataTableView: TableEntryView<Phase1DataEntry> by Delegates.notNull()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_indoor_resting_collection)
+        val gson = Gson()
+        val metaDataBundle = intent.getBundleExtra("MetaData")
+        val tableBundle = intent.getBundleExtra("DataTableBundle")
+
+        val dataTable = if (metaDataBundle != null) {
+            Phase1DataTable(metaDataBundle.getParcelable("MetaData") as Phase1MetaData,
+                    4)
+        } else {
+            gson.fromJson(tableBundle.getString("DataTableString"), Phase1DataTable::class.java)
+        }
+        this.metaData = dataTable.metaData
+        this.DataTableView = TableEntryView<Phase1DataEntry>(this, dataTable)
+        this.customHeaderTableFormat()
+        val dataTableContainer = findViewById<LinearLayout>(R.id.MainTableContainer)
+        dataTableContainer.addView(this.DataTableView, 0)
+
+        var saveButton = findViewById<Button>(R.id.collectDataButton)
+        var completeButton = findViewById<Button>(R.id.completeButton)
+        if (this.metaData.sent) {
+            saveButton.visibility = View.GONE
+            completeButton.visibility = View.GONE
+        }
+
+        var serialNumberLabel = findViewById<TextView>(R.id.serial_label)
+        serialNumberLabel.text = "serial: " + this.metaData.serial.toString()
+    }
+
+    fun completeForm(view: View) {
+
+        if (this.DataTableView.ensureNoNull(this.DataTableView)) {
+            this.DataTableView.tableData.metaData.completed = true
+            this.DataTableView.tableData.metaData.sent = false
+            writeData2Json(this.DataTableView.tableData as Phase1DataTable)
+            var intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish()
+            startActivity(intent)
+        } else {
+            this.DataTableView.alertMissingData()
+        }
+    }
+
+    override fun onBackPressed() {
+        val gson = Gson()
+        var returnIntent = Intent()
+
+        returnIntent.putExtra("result", gson.toJson(this.DataTableView.tableData as Phase1DataTable))
+        setResult(Activity.RESULT_OK, returnIntent)
+        finish()
+    }
+
+    fun saveFormForTomorrow(view: View) {
+        this.DataTableView.tableData.metaData.completed = false
+        this.DataTableView.tableData.metaData.sent = false
+        writeData2Json(this.DataTableView.tableData as Phase1DataTable)
+        var intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        finish()
+        startActivity(intent)
+    }
+
+    fun customHeaderTableFormat() {
+        val verticalHeader1 = findViewById<TableRow>(R.id.VerticalHeader1)
+        val verticalHeader2 = findViewById<TableRow>(R.id.VerticalHeader2)
+        val colWidthUnscaled = this.DataTableView.getColWidth()[0]
+        val colWidthScaled = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, colWidthUnscaled, this.resources.displayMetrics))
+
+        for (iCol in 0 until verticalHeader1.childCount) {
+            var thisHeaderCell = verticalHeader1.getChildAt(iCol)
+            thisHeaderCell.layoutParams.width = colWidthScaled
+        }
+        for (iCol in 0 until verticalHeader2.childCount) {
+            var thisHeaderCell = verticalHeader2.getChildAt(iCol)
+            thisHeaderCell.layoutParams.width = colWidthScaled
+        }
+    }
+
+    fun writeData2Json(data: Phase1DataTable) {
+        if (this.metaData.count == formCountTracker.readFormCount(this.metaData.formType, this)) {
+            formCountTracker.increaseFormCount(this.metaData.formType, this)
+        }
+        val gson = Gson()
+        val jsonString: String = gson.toJson(data)
+        val fsu = FileStoreUtil()
+        val tsLong = System.currentTimeMillis()
+        val ts = tsLong.toString()
+        val filename = this.metaData.getFilename()
+        writeToFile(jsonString, filename, getApplicationContext())
+        println(this.getFilesDir().getAbsolutePath().toString())
+    }
+
+    fun writeToFile(data: String, filename: String, context: Context) {
+        try {
+            val outputStreamWriter = OutputStreamWriter(context.openFileOutput(filename, Context.MODE_PRIVATE))
+            outputStreamWriter.write(data)
+            outputStreamWriter.close()
+        } catch (e: Exception) {
+            Log.e("Exception", "File write failed: " + e.toString())
+        }
+
+    }
 }
--- app/src/main/java/com/example/kaftand/entomologydatacollect/Phase1/Phase1Intro.kt
+++ app/src/main/java/com/example/kaftand/entomologydatacollect/Phase1/Phase1Intro.kt
@@ -20,4 +20,167 @@ import kotlin.properties.Delegates
 import kotlin.reflect.KMutableProperty0
 
 class Phase1Intro : LanguagePreservingActivity(){
+    var responsibleForData = false
+    var metaData: Phase1MetaData by Delegates.notNull()
+    var dataTable: Phase1DataTable? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        metaData = Phase1MetaData(formCountTracker.getSerialNumber(this@Phase1Intro, FormTypeKeys.IndoorRestingCollection))
+        setContentView(R.layout.content_indoor_resting_collection_intro)
+        this.metaData.count = formCountTracker.readFormCount(metaData.formType, this)
+
+        val textView: TextView = findViewById(R.id.DateEdit)
+        textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+        var cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            textView.text = sdf.format(cal.time)
+            this.metaData.DATE = sdf.format(cal.time)
+        }
+        val myFormat = "dd.MM.yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        this.metaData.DATE = sdf.format(cal.time)
+
+        textView.setOnClickListener {
+            DatePickerDialog(this@Phase1Intro, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        val projectCodeTextEdit = findViewById<EditText>(R.id.project_code)
+        projectCodeTextEdit.setText(this.metaData.PROJECT_CODE)
+        projectCodeTextEdit.isEnabled = false
+        val clusterNumberTextEdit = findViewById<EditText>(R.id.cluster_number_cdc)
+        clusterNumberTextEdit.addTextChangedListener(createTextWatcherInt(createCallBackFor<Int?>(this.metaData::CLUSTER_NUMBER)))
+        val volunteerNumberTextEdit = findViewById<AutoCompleteTextView>(R.id.volunteer_number)
+        volunteerNumberTextEdit.addTextChangedListener(createTextWatcherString(createCallBackFor<String?>(this.metaData::VOLUNTEER)))
+        val monthTextEdit = findViewById<EditText>(R.id.month)
+        monthTextEdit.addTextChangedListener(createTextWatcherInt(createCallBackFor<Int?>(this.metaData::MONTH)))
+        val weekTextEdit = findViewById<EditText>(R.id.week)
+        weekTextEdit.addTextChangedListener(createTextWatcherInt(createCallBackFor<Int?>(this.metaData::WEEK)))
+
+        val spinner = findViewById(R.id.village_spinner) as Spinner
+        val adapter = ArrayAdapter.createFromResource(this,
+                R.array.villages_drop_down, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.setAdapter(adapter)
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                updateVillage(parent as Spinner)
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
+        val volunteerAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, volunteerNameTracker.readVolunteerNames(this))
+        volunteerNumberTextEdit.setAdapter(volunteerAdapter)
+    }
+
+
+    fun updateVillage(spinner: Spinner) {
+        if(R.string.tap_to_enter.toString() == spinner.selectedItem.toString()) {
+            this.metaData.VILLAGE = null
+        }
+        this.metaData.VILLAGE = spinner.selectedItem.toString()
+    }
+
+
+    fun nextScreen(view: View) {
+        try {
+            if (!checkMissingDataInLayout.isDataMissingSetError(findViewById(R.id.main_container))) {
+                if ((this.metaData.VOLUNTEER != null) and (this.metaData.VOLUNTEER != "") ){
+                    volunteerNameTracker.appendNameIfUnique(this.metaData.VOLUNTEER!!, this)
+                }
+                var intent = Intent(this, Phase1::class.java)
+                var bundle: Bundle = Bundle()
+                if (responsibleForData) {
+                    val gson = Gson()
+                    updateDataTableMeta()
+                    bundle.putString("DataTableString", gson.toJson(this.dataTable!!, Phase1DataTable::class.java))
+                    intent.putExtra("DataTableBundle",bundle)
+                    startActivityForResult(intent, 1)
+                } else {
+                    bundle.putParcelable("MetaData", this.metaData)
+                    intent.putExtra("MetaData", bundle)
+                    startActivityForResult(intent, 1)
+                }
+            }
+        } catch (e : Exception) {
+            return
+        }
+    }
+
+    fun updateDataTableMeta() {
+        for (iRow in 0 until this.dataTable!!.dataArray.size) {
+            this.dataTable!!.dataArray[iRow].updateFromMetaData(this.metaData)
+        }
+        this.dataTable!!.metaData = this.metaData
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                val gson = Gson()
+                this.dataTable = gson.fromJson(data!!.getStringExtra("result"), Phase1DataTable::class.java)
+                this.responsibleForData = true
+            }
+        } else {
+            finish()
+        }
+    }
+
+
+    fun <dataT>createCallBackFor(property: KMutableProperty0<dataT>) : (dataT) -> (Unit) {
+        val callback = fun(count: dataT): Unit { property.set(count)}
+        return callback
+    }
+
+    fun createTextWatcherInt(function : (Int?) -> (Unit)) : TextWatcher {
+        return object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                function(s.toString().toIntOrNull())
+            }
+        }
+    }
+
+    fun createTextWatcherNonNullInt(function : (Int) -> (Unit)) : TextWatcher {
+        return object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                val output = if (s.toString().toIntOrNull() == null) {0} else {s.toString().toInt()}
+                function(output)
+            }
+        }
+    }
+
+    fun createTextWatcherString(function : (String?) -> (Unit)) : TextWatcher {
+        return object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                function(s.toString())
+            }
+        }
+    }
+
 }
